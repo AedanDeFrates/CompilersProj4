60aeb2a5949b83e6cf5d3e7027ae6773453afc21
doublea9257 <aahawk@cox.net>
2026-05-03 19:18:50 -0500

    Add files via upload
    added small duplicate fix, added documentation and comments

--- CodeGen/CreateFuncPass.java
+++ CodeGen/CreateFuncPass.java
@@ -8,15 +8,22 @@ public class CreateFuncPass extends CodeGenPass<Void>{
         super(p, s);
     }
 
+    /*
+    This creates an empty GOTO Function object for each function declaration it passes in the AST.
+    Instructions are added later in the Instruction Pass
+     */
     // Creates a new Function in the program
     @Override
     public Void visitFunDecl(FunDecl node) {
         System.out.println("CREATE_FUNC_PASS visitFunDecl\n   " + node.name);
 
+        //moves into function's scope
         switchScope(node,()->{
             String name = node.name;
             String returnType = node.type.name;
+            //creates a GOTO function shell with empty instructions.
             Function func =  new Function(name,returnType);
+            //adds the function to the program
             pm.program.funcs.add(func);
 
             visit(node.type);
